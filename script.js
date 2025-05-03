4fbcc869de3e9a0bb5b76800da90f30dbfcef92f
Lewis1026 <lewis_tiong28@yahoo.com>
2025-05-03 20:05:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function calculateMortgage() {
-    const principal = parseFloat(document.getElementById('principal').value);
-    const interestRate = parseFloat(document.getElementById('interest').value) / 100 / 12;
-    const payments = parseInt(document.getElementById('years').value) * 12;
-    const x = Math.pow(1 + interestRate, payments);
-    const monthly = (principal * x * interestRate) / (x - 1);
-    if (!isNaN(monthly)) {
-      document.getElementById('result').textContent = 'Monthly Payment: $' + monthly.toFixed(2);
-    } else {
-      document.getElementById('result').textContent = 'Please enter valid values.';
-    }
+  const principal = parseFloat(document.getElementById('principal').value);
+  const interestRate = parseFloat(document.getElementById('interest').value) / 100 / 12;
+  const payments = parseInt(document.getElementById('years').value) * 12;
+  const x = Math.pow(1 + interestRate, payments);
+  const monthly = (principal * x * interestRate) / (x - 1);
+  if (!isNaN(monthly)) {
+    document.getElementById('result').textContent = 'Monthly Payment: $' + monthly.toFixed(2);
+  } else {
+    document.getElementById('result').textContent = 'Please enter valid values.';
   }
-  
+}
